b4de9d29b35fe94ae8f0a2c1328f8a8588d7fd26
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-09-07 23:25:46 +0530

    Added navbar and custom cursor

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// !cursor
+let mouseCursor = document.querySelector('.cursor')
+let navLinks = document.querySelectorAll('.nav-links li')
+
+window.addEventListener('mousemove', cursor)
+
+function cursor(e) {
+	mouseCursor.style.top = e.pageY + 'px'
+	mouseCursor.style.left = e.pageX + 'px'
+}
+
+navLinks.forEach((link) => {
+	link.addEventListener('mouseover', () => {
+		mouseCursor.classList.add('link-grow')
+	})
+	link.addEventListener('mouseleave', () => {
+		mouseCursor.classList.remove('link-grow')
+	})
+})
+
+// ! navbar
+const navSlide = () => {
+	const burger = document.querySelector('.burger')
+	const nav = document.querySelector('.nav-links')
+	const navLinks = document.querySelectorAll('.nav-links li')
+	// Toggle nav
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active')
+		//animate links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = ''
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${
+					index / 5 + 0.3
+				}s`
+			}
+		})
+		//burger animation
+		burger.classList.toggle('toggle')
+	})
+}
+
+navSlide()
